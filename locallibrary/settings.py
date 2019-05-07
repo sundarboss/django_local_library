4984601638a3ae3da59aee9846537b3079b448c7
sundarboss <s.sundar_100@yahoo.com>
2019-05-07 19:16:20 +0530

    Update allowed host with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm6w3eu#-n=rt*mse$vs36cbob+46flg8+ale!p4hyi&jon)qm1'
+# SECRET_KEY = 'm6w3eu#-n=rt*mse$vs36cbob+46flg8+ale!p4hyi&jon)qm1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'm6w3eu#-n=rt*mse$vs36cbob+46flg8+ale!p4hyi&jon)qm1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evening-waters-79974.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
